8e19e2aa5379a7d6d5c21f11c890b5e74515faee
MrMariu5 <marius.sadreika@gmail.com>
2020-05-16 15:55:07 +0300

    Work with simple site

--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -1,65 +1,110 @@
-﻿using RestSharp;
-using System;
+﻿using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Web.Configuration;
 
 namespace SecondTryCrawler
 {
     public class Program
     {
+        public int triesToConnect = 0;
+        public int proxy_number = 0;
         public String proxyChange()
         {
             String proxy = "";
             try
             {
                 string[] lines = System.IO.File.ReadAllLines(@"C:\Users\mariu\Documents\GitHub\New\WebCrawlertest0.1\WebCrawlerv0.1\Proxy.txt"); // proxiu sarasas
-                proxy = lines[3];
+                proxy = lines[proxy_number];
             }
             catch (Exception fileException)
             {
-                Console.WriteLine("Cant find the file");
+                Console.WriteLine("FILE ERROR");
                 proxy = null;
             }
             return proxy;
-            //String new_proxy = proxyChange();
-            //string[] proxyInfo = new_proxy.Split(':');
-            //client.Proxy = new WebProxy(proxyInfo[0], Int32.Parse(proxyInfo[1]));
         }
+
 
         public int crawling(String urlAddress)
         {
+            String new_proxy = proxyChange();
+            if (new_proxy == null)
+            {
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                if (triesToConnect == 2)
+                {
+                    proxy_number = proxy_number + 1;
+                    new_proxy = proxyChange();
+                    if (new_proxy == null)
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        triesToConnect = 0;
+                    }
+                }
+                Console.WriteLine((triesToConnect + 1) + " TRY");
+            }
 
-            RestClient client = new RestClient(urlAddress);
-            client.AddDefaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0;)");
-            client.AddDefaultHeader("Accept-Language", "en-GB,en;q=0.5");
-            //client.AddDefaultHeader("Accept-Encoding", "gzip, deflate, br"); */
-            client.AddDefaultHeader("Accept", "text / html,application / xhtml + xml,application / xml; q = 0.9,image / webp,*/*;q=0.8");
-            var cookieContainer = new CookieContainer();
-            cookieContainer.Add(new Cookie("__cfduid", "d817e3346238b447472c70cf62558b2fd1589259908", "/", "www.norwegian.com"));
-            cookieContainer.Add(new Cookie("cf_clearance", "8d93bdf19e18771df2f57d6b92ce47b8167ba87c-1589563029-0-250", "/", "www.norwegian.com"));
-            client.CookieContainer = cookieContainer;
+            WebClient client = new WebClient();
 
-            var request = new RestRequest("", Method.GET);
+            client.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:76.0) Gecko/20100101 Firefox/76.0");
+            
+            client.Headers.Set("Accept-Language", "en-GB,en;q=0.5");
+            client.Headers.Set("Accept", "application/json, text/plain, */*");
+
+            
+            Console.WriteLine("INFO ABOUT HEADER\n" + client.Headers);
+
+            string[] proxyInfo = new_proxy.Split(':');
+            client.Proxy = new WebProxy(proxyInfo[0], Int32.Parse(proxyInfo[1]));
+            Console.WriteLine("PROXY\n" + proxyInfo[0] + " " + Int32.Parse(proxyInfo[1]));
 
             try
             {
-                var response = client.Execute(request).Content;
-                Console.WriteLine(response);
+                string data = client.DownloadString(urlAddress);
+
+                WebHeaderCollection myWebHeaderCollection = client.ResponseHeaders;
+                for (int i = 0; i < myWebHeaderCollection.Count; i++)
+                    Console.WriteLine("\t" + myWebHeaderCollection.GetKey(i) + " = " + myWebHeaderCollection.Get(i));
+
+                Console.WriteLine("Duomenys\n" + data);
+                return 0;
+
             }
             catch (Exception response_exception)
             {
-                Console.WriteLine("Error");
+                triesToConnect = triesToConnect + 1;
+                Console.WriteLine("ERROR\n" + response_exception);
+                return 1;
             }
-            return 0;
         }
 
         static void Main(string[] args)
         {
             int methodRecursion = 1;
             Program crawlerObject = new Program();
-            string urlAddress = "https://www.norwegian.com/uk/";
-            methodRecursion = crawlerObject.crawling(urlAddress);
+            string urlAddress = "https://www.ezys.lt/";
+            while (methodRecursion == 1)
+            {
+                methodRecursion = crawlerObject.crawling(urlAddress);
+            }
+            Console.WriteLine("END");
         }
     }
 }
+// client.Headers.Set("Accept-Encoding", "gzip, deflate, br");
+// client.Headers.Set(HttpRequestHeader.Cookie, "MUID=");
+// client.Headers.Set(HttpRequestHeader.Cookie, "cf_clearance=");
+// client.Headers.Set(HttpRequestHeader.Cookie, "__cfduid=");
+// client.Headers.Set(HttpRequestHeader.Cookie, "ASP.NET_SessionId=");
+// client.Headers.Set("__RequestVerificationToken", "X0kk1RIYeYSnHf3iX-tS1E__veNA1-F9JUXk0D-kFjsjDM17QOyEV8zWHWWtO5svxzSAjTcKYLwMXDn46e-rAN39EEk1");
+// client.Headers.Set("forterToken", "c472c17dbe8a458f8babc3299ba99403_1589618250040_15450_UDF43_9ck");
